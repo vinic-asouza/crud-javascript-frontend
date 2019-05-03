25b27b4eb478e5a6bb2853f0d91dd15bb2778997
Prisma MBP 103 <prisma@Assis.local>
2019-05-03 16:30:13 -0300

    Finalizado

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -5,5 +5,5 @@ export default props =>
     <Main icon="home" title="Inicio" subtitle="Projeto">
         <div className="display-4">Bem Vindo!</div>
         <hr />
-        <p className="mb-0">Sistem apara exemplicificar a contrução de um cadastro desenvolvido em React!</p>
+        <p className="mb-0">Sistema para exemplicificar a contrução de um cadastro desenvolvido em React!</p>
     </Main>
--- src/main/App.jsx
+++ src/main/App.jsx
@@ -2,16 +2,19 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
+import Routes from './Routes'
 import Logo from '../components/template/Logo'
-import Home from '../components/home/home'
 import Footer from '../components/template/Footer'
 import Nav from '../components/template/Nav'
 
 export default props =>
-    <div className="app">
-        <Logo />
-        <Nav />
-        <Home />
-        <Footer />
-    </div>
+    <BrowserRouter>
+        <div className="app">
+            <Logo />
+            <Nav />
+            <Routes />
+            <Footer />
+        </div>
+    </BrowserRouter>
